198a41fecb5b00a6d061440a5fad8d85a8c423e6
Dazikiri Anikar <mierzwinski.b@gmail.com>
2020-12-07 19:51:22 +0100

    Added quote generating architecture.

---
+++ src/main/java/bm/app/repositories/Sentences.java
@@ -0,0 +1,20 @@
+package bm.app.repositories;
+
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+
+@Component
+public class Sentences {
+
+    public static ArrayList<String> sentenceList = new ArrayList<>();
+
+    static {
+        sentenceList.add(0, "You look very nice today.");
+        sentenceList.add(1, "I hope all your dreams comes true.");
+        sentenceList.add(2, "May your hardships be few.");
+        sentenceList.add(3, "You will make it. Don't give up.");
+        sentenceList.add(4, "You deserve to be happy.");
+        sentenceList.add(5, "You're making this world a better place/");
+    }
+}
---
+++ src/main/java/bm/app/tasks/ScheduledTask.java
@@ -0,0 +1,30 @@
+package bm.app.tasks;
+
+import bm.app.repositories.Sentences;
+import org.springframework.stereotype.Component;
+
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+@Component
+public class ScheduledTask {
+
+    Sentences sentences;
+
+    public ScheduledTask(Sentences sentences){
+        this.sentences = sentences;
+    }
+
+    public void saySomethingNice(){
+        System.out.println(quotePuller());
+    }
+
+    private String quotePuller(){
+        return sentences.sentenceList.get(indexGenerator());
+    }
+
+    private int indexGenerator(){
+        int maxNumber = sentences.sentenceList.size();
+        return ThreadLocalRandom.current().nextInt(0, maxNumber + 1);
+    }
+}
